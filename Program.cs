536007e428b44a5521e19d43fd1aaea68b18bd5a
Erik Scofield <erikscf@clearfunction.com>
2023-11-21 11:23:20 -0600

    Initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/climatechange", () => {
+  string[] Summaries = new[]
+  {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  };
+  return Enumerable.Range(1, 5).Select(index => new
+  {
+      Date = DateTime.Now.AddDays(index),
+      TemperatureC = Random.Shared.Next(-20, 55),
+      Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+  })
+  .ToArray(); 
+});
+
+app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GPTApi
+
+
+to deploy, run the task azure publish
+
+requirements:
+
+azure cli:
+
+https://aka.ms/installazurecliwindowsx64
